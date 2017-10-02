2ff3c17d74566e0b2bf4e103814f9bd09bb3fd0b
Cameron Foster <cameronfoster46@gmail.com>
2017-10-02 22:53:11 +0100

    Added basic functionality

--- classes-practice.py
+++ classes-practice.py
@@ -1,13 +1,48 @@
 class Employee:
+    raise_amount = 1.05
+
     def __init__(self, first_name, last_name, pay):
         self.first_name = first_name.title()
         self.last_name = last_name.title()
         self.pay = pay
         self.email = f"{self.first_name.lower()}{self.last_name.lower()}@bogus.com"
 
+    def apply_raise(self):
+        self.pay *= self.raise_amount
 
-emp1 = Employee("Corey", "Schafer", 50000)
 
-print(emp1.first_name)
-print(emp1.last_name)
-print(emp1.email)
+emp1 = Employee("John", "Doe", 50000)
+emp2 = Employee("Jane", "Smith", 70000)
+
+emps = [emp1, emp2]
+
+for i in emps:
+    print(i.first_name)
+    print(i.last_name)
+    print(i.email)
+    print(i.pay)
+    print()
+
+print('{:,.2f}'.format(emp1.pay))
+emp1.apply_raise()
+print('{:,.2f}'.format(emp1.pay))
+
+print()
+
+print('{:,.2f}'.format(emp2.pay))
+emp2.apply_raise()
+print('{:,.2f}'.format(emp2.pay))
+
+print()
+
+Employee.raise_amount = 1.10
+
+print('{:,.2f}'.format(emp1.pay))
+emp1.apply_raise()
+print('{:,.2f}'.format(emp1.pay))
+
+print()
+
+print('{:,.2f}'.format(emp2.pay))
+emp2.apply_raise()
+print('{:,.2f}'.format(emp2.pay))
