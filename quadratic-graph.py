b6bfb749cfc862836715de05aed1a4b23d195e8e
Cameron Foster <cameronfoster46@gmail.com>
2017-10-07 19:19:37 +0100

    Improved y-axis display

--- quadratic-graph.py
+++ quadratic-graph.py
@@ -16,7 +16,14 @@ for x in x_array:
 # plot data
 plt.plot(x_array, y_array, label="graph")  # plots the graph of the function
 plt.plot([max(x_array), min(x_array)], [0, 0], label="x-axis")  # plots the x-axis
-plt.plot([0, 0], [max(y_array), -(max(y_array))], label="y-axis")  # plots the y-axis
+
+# if a is positive, y-axis is drawn up to the highest value of f(x)
+if a > 0:
+    plt.plot([0, 0], [max(y_array), -(max(y_array))], label="y-axis")  # plots the y-axis
+
+# if a is negative, y-axis is drawn up to the absolute value of the lowest value of f(x)
+elif a < 0:
+    plt.plot([0, 0], [abs(min(y_array)), min(y_array)], label="y-axis")  # plots the y-axis
 
 # configure graph
 plt.title(f"Graph of f(x) = {a}x^2 + {b}x + {c}")
